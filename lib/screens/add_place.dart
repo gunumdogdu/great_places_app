efdff460b60508bd13fbf65fb4eb093ca519c6db
gunumdogdu <emirgunumdogdu@gmail.com>
2023-04-12 00:19:16 +0300

    Location feature

--- lib/screens/add_place.dart
+++ lib/screens/add_place.dart
@@ -4,6 +4,7 @@ import 'dart:io';
 
 import 'package:great_places_app/providers/user_places.dart';
 import 'package:great_places_app/widgets/image_input.dart';
+import 'package:great_places_app/widgets/location_input.dart';
 
 class AddPlaceScreen extends ConsumerStatefulWidget {
   const AddPlaceScreen({super.key});
@@ -61,6 +62,8 @@ class _AddPlaceScreenState extends ConsumerState<AddPlaceScreen> {
                 _selectedImage = image;
               },
             ),
+            const SizedBox(height: 10),
+            LocationInput(),
             const SizedBox(height: 16),
             ElevatedButton.icon(
               onPressed: _savePlace,
